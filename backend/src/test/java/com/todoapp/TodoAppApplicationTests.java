1ff5880a12260d9af7eb9d0f875c512c7340a353
Umut Arslan <umut.arslan@klosebrothers.de>
2023-02-03 12:05:32 +0100

    add TodoController Test

--- backend/src/test/java/com/todoapp/TodoAppApplicationTests.java
+++ backend/src/test/java/com/todoapp/TodoAppApplicationTests.java
@@ -1,14 +1,32 @@
 package com.todoapp;
 
+import com.todoapp.todo.TodoController;
+import com.todoapp.todo.TodoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
 class TodoAppApplicationTests {
 
+	@Autowired
+	TodoService todoservice;
+	MockMvc mockMvc;
+
+	@BeforeEach
+	void setup() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new TodoController(todoservice)).build();
+	}
+
 	@Test
-	void contextLoads() {
+	void TodoControllerTest() throws Exception {
+		mockMvc.perform(get("/api/v1/todos").accept(MediaType.APPLICATION_JSON));
 	}
 
 }
