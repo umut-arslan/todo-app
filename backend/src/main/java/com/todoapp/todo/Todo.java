ed66e843b0f35a0ca4dddc4291aa0de338e5e570
Umut Arslan <umut.arslan@klosebrothers.de>
2023-03-01 15:02:37 +0100

    add h2 dependency and annotations

--- backend/src/main/java/com/todoapp/todo/Todo.java
+++ backend/src/main/java/com/todoapp/todo/Todo.java
@@ -1,5 +1,6 @@
 package com.todoapp.todo;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
@@ -7,7 +8,19 @@ import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
+@Table
 public class Todo {
+    @Id
+    @SequenceGenerator(
+            name = "todo_sequence",
+            sequenceName = "todo_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
     @Getter @Setter
     private Long id;
     @Getter @Setter
